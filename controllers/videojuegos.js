dab4d0a5db8c3d60170d09b09f88ef065cb72223
sahuer <sahuer.daniel@gmail.com>
2021-10-04 16:52:20 -0500

    Videojuegos1

--- controllers/videojuegos.js
+++ controllers/videojuegos.js
@@ -1,11 +1,6 @@
 const path = require('path')
 const Videojuego = require('../utils/database').models.videojuego
 
-/*
-exports.getAgregarVideojuego = (req,res)=>{
-    res.send('Formulario')
-}
-*/
 
 exports.postAgregarVideojuego = (req,res)=>{
     console.log(req.body);      // <= {id: number, nombre: text}
@@ -18,14 +13,8 @@ exports.postAgregarVideojuego = (req,res)=>{
             console.log(err)
             res.json({estado: "error"})
         })
-    // res.redirect('/videojuegos/confirmacionDatos')
 }
 
-/*
-exports.getConfirmacionDatos = (req,res)=>{
-    res.send('Confirmacion')
-}
-*/
 
 exports.getObtenerVideojuegos = (req,res)=>{
     Videojuego.findAll()
