ebcb53f1e2f1de8eb8769e3064e1524fc534e733
sahuer <sahuer.daniel@gmail.com>
2021-10-04 16:29:26 -0500

    Videojuegos1

--- routes/videojuegos.js
+++ routes/videojuegos.js
@@ -4,14 +4,8 @@ const vjController = require('../controllers/videojuegos');
 const router = express.Router();
 
 
-    // Formulario de ingreso de datos (FRONT-END)
-    // router.get('/agregarVideojuego',vjController.getAgregarVideojuego)
-
 // Servicio para el procesamiento de datos
 router.post('/agregarVideojuego',vjController.postAgregarVideojuego)
-
-    // Pagina de confirmacion (FRONT-END)
-    // router.get('/confirmacionDatos',vjController.getConfirmacionDatos)
     
 //Visualización de los datos guardados
 router.get('/obtenerVideojuegos',vjController.getObtenerVideojuegos)
